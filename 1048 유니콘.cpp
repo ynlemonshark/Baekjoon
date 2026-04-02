91f8ed39b8fc9c20df601a8a993358094aca8541
YeonuShin <101554651+ynlemonshark@users.noreply.github.com>
2026-04-02 12:41:10 +0900

    Refactor and fix issues in 1048 유니콘.cpp

--- 1048 유니콘.cpp
+++ 1048 유니콘.cpp
@@ -1,30 +1,58 @@
-// [1048]유니 PLAT4
+// [1048]유니콘 PLAT4
 
 #include <iostream>
 #include <vector>
 #include <string>
 
+using namespace std;
 
 
-vector<char> str;
-str.reserve(51);
+int n, m, l;
 
-vector<int> index[26];
+vector<char> ustr;
+
+vector<int> mindex[26];
 
 vector<long long int> dp[50];
 
-using namespace std;
-
-long long int rec(int depth, vector<int>& str, )
+int abs(int a)
 {
+    return (a<0)?-a:a;
+}
+
+int goable(int dpt, int dst)
+{
+    int dx = abs((dpt % m) - (dst % m));
+    int dy = abs((dpt / m) - (dst / m));
+    if (dx + dy > 4 & dx > 1 & dy > 1)
+        return 1;
+    else
+        return 0;
+}
+
+long long int rec(int depth, int idx)
+{
+    long long int sum = 0;
+    if (dp[depth].empty())
+    {
+        for (int i=0; i<mindex[depth].size(); i++)
+        {
+            dp[depth].push_back(rec(depth + 1, i));
+        }
+    }
     
+    for (int i=0; i<mindex[depth].size(); i++)
+    {
+        if (goable(mindex[depth][i], mindex[depth][idx]))
+            sum += dp[depth][i];
+    }
+    return sum % 1000000007;
 }
 
 int main() {
     ios::sync_with_stdio(false);
-    cin::tie(NULL);
+    cin.tie(NULL);
 
-    int n, m, l;
     scanf("%d %d %d\n", &n, &m, &l);
     
     vector<char> board;
@@ -33,11 +61,12 @@ int main() {
     scanf("%c", &c);
     while (c != '\n')
     {
-        str.push_back(c);
+        ustr.push_back(c);
         scanf("%c", &c);
     }
-    str.push_back('\0');
-    int len = str.size() - 1;
+    int len = ustr.size() - 1;
+    
+    board.reserve(n*m);
 
     char trash;
     for (int i=0; i<n; i++)
@@ -46,12 +75,19 @@ int main() {
         {
             scanf("%c", &c);
             board.push_back(c);
-            index[c-'a'].push_back(i*m + j);
+            mindex[c-'A'].push_back(i*m + j);
         }
         scanf("%c", &trash);
     }
-    
-    board.reserve(n*m);
-    
+
+    dp[len - 1].resize(mindex[ustr.back() - 'A'].size(), 1);
+
+    long long int sumall;
+    for (int i=0; i<mindex[0].size(); i++)
+    {
+        sumall += rec(0, i);
+    }
+    sumall % 1000000007;
+    printf("%lld", sumall);
     return 0;
 }
